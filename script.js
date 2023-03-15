281751142ad72512a23181a66837ed2631dc64c1
rafaelgs13 <110254029+rafaelgs13@users.noreply.github.com>
2023-03-15 23:09:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var btn = document.querySelector('#show-or-hide');
+var container = document.querySelector('.caixa_branca');
+
+btn.addEventListener('click', function() {
+	
+if(container.style.display === 'block') { 
+container.style.display = 'none';
+} else {
+container.style.display = 'block';
+}
+
+});
+
+
+//Planta
+
+var btn = document.querySelector('#show-or-hide1');
+var container = document.querySelector('.caixa_branca');
+
+btn.addEventListener('click', function() {
+	
+if(container.style.display === 'block') { 
+container.style.display = 'none';
+} else {
+container.style.display = 'block';
+}
+
+});
+
+
+//nav bar
+
